ec62ab00262a698b0077d277881430ad29f40935
karimaart <rtibikarima20@gmail.com>
2023-10-19 12:25:11 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,90 @@
+export const data = [
+  {
+    id: "gjfjgvuvjgjj",
+    imgUrl:
+      "https://m.media-amazon.com/images/M/MV5BMDc3YzNiYTgtMWJmMC00OWI4LWJlZTMtNmY5YTNmN2Q0MzlkXkEyXkFqcGdeQXVyMzI4MjI5NTE@._V1_.jpg",
+    title: "Barbie & Chelsea: The Lost Birthday",
+    rating: 5,
+    desc: `Barbie's younger sister Chelsea is trying to celebrate her 7th birthday but her birthday is skipped due to the international date line..`,
+    trailer: "https://www.youtube.com/embed/2xU6_8Z0Ohc?si=KerwWzlRkvV80bQv",
+  },
+  {
+    id: "NDIDNIDDID",
+    imgUrl:
+      "https://m.media-amazon.com/images/M/MV5BNzU2MWJkYjQtNzUwYy00YjZmLWI4ZTEtMjVkZWQ2OGJlN2Q4XkEyXkFqcGdeQXVyNzQyNTU2MjI@._V1_.jpg",
+    title: "Barbie Princess Adventure",
+    rating: 7,
+    desc: "With new friends in a new kingdom, Barbie learns what it means to be herself when she trades places with a royal lookalike in this musical adventure.",
+    trailer: "https://www.youtube.com/embed/7joR5V_T3wQ?si=JcELeplQ1kY-zSHt",
+  },
+  {
+    id: "WIFJWLWROOF",
+    imgUrl: "https://fr.web.img5.acsta.net/pictures/17/08/25/11/58/527522.jpg",
+    title: "Barbie and the Magic of the Dolphins",
+    rating: 6,
+    desc: "Barbie and her sisters visit a sun-kissed island resort, only to discover that a rare Gemstone dolphin has gone missing. Will Barbie and her mysterious new friend Isla manage to find it? Above all, what is Isla's well-hidden secret?",
+    trailer: "https://www.youtube.com/embed/ljbPuwvn4fY?si=zFIy9WMag0kjTYEK",
+  },
+  {
+    id: "DEDIOCKE",
+    imgUrl:
+      "https://m.media-amazon.com/images/M/MV5BNjhjYWZlNDctZGE3MS00NDYzLThmNDctZDc1ZGRkY2FlZWU1XkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_.jpg",
+    title: "Barbie: Rock and Royals",
+    rating: 6,
+    desc: "Get ready to rock! In this upbeat musical, Barbie stars as Courtney, a princess whose world is turned upside down when she switches places with Erika, a famous rockstar.",
+    trailer: "https://www.youtube.com/embed/qehwVspSpmw?si=SEnuwLvRWyuXJnJG",
+  },
+  {
+    id: "FEOJCVOP",
+    imgUrl: "https://imgsrc.cineserie.com/2016/08/1553228-1.jpg?ver=1",
+    title: "Barbie : Agents secrets",
+    rating: 6,
+    desc: "Barbie and her athletic best friends slip into their smart outfits when a secret intelligence agency wants to use their special skills to stop a series of gem robberies. Will the Spy Squad succeed in this difficult cat-and-mouse game?",
+    trailer: "https://www.youtube.com/embed/0w35exFoQIk?si=8qtHspfogRGflvHl",
+  },
+  {
+    id: "IIRJDJIOE",
+    imgUrl:
+      "https://m.media-amazon.com/images/M/MV5BZTE3M2I5MGYtNTYwMC00NTRmLTk5OWYtNWI0YzM0MmVmNzk0XkEyXkFqcGdeQXVyOTI2MTMwMDc@._V1_.jpg",
+    title: "Dora the Explorer",
+    rating: 4,
+    desc: "Dora goes on an adventure with her friend Boots the monkey.",
+    trailer: "https://www.youtube.com/embed/gGslvfeQocU?si=C5pmNq-Eu7--zDFc",
+  },
+  {
+    id: "EOPOEDV",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuRudQb429yyPIASKpkJngWep2Dp0uo0FZeg&usqp=CAU",
+    title: "Mickey's house",
+    rating: 6,
+    desc: "Mickey and his friends Minnie, Donald, Pluto, Daisy, Goofy and many others embark on fun and educational adventures.",
+    trailer: "https://www.youtube.com/embed/JkcxEUNohC8?si=QGnQZFobRFPBqurk",
+  },
+  {
+    id: "DUHIEFUE",
+    imgUrl:
+      "https://m.media-amazon.com/images/M/MV5BOWY0YjIzYzctNGQ2My00ZTU2LWE3MTQtZGIyZWFhMmUyYzQ1XkEyXkFqcGdeQXVyMjM5NDQzNTk@._V1_.jpg",
+    title: "Bubble Guppies",
+    rating: 7,
+    desc: "Jump into this underwater class with Molly, Gil and their finned friends. Students can prepare for school in subjects like science, math, reading and more through song, dance and laughter!",
+    trailer: "https://www.youtube.com/embed/5nZWJCPBdAE?si=_hxkA_dHcd3Cxy9O",
+  },
+  {
+    id: "DOWCBXB",
+    imgUrl:
+      "https://images.justwatch.com/poster/173662803/s718/martha-speaks.jpg",
+    title: "Martha Speaks",
+    rating: 3,
+    desc: "Tales of a dog, Martha, who eats alphabet soup and gains the ability to speak. This series follows the antics of Martha and her owner as she encounters many adventures with her newfound ability.",
+    trailer: "https://www.youtube.com/embed/UIMdQBO6ZPg?si=bphJ89HH1IFZq3Wd",
+  },
+  {
+    id: "OWHRCXSB",
+    imgUrl:
+      "https://m.media-amazon.com/images/I/812YIb+PhhL._AC_UF1000,1000_QL80_.jpg",
+    title: "Clifford the big red dog",
+    rating: 6,
+    desc: "The adventures of a larger-than-life red dog on Bridwell Island.",
+    trailer:"https://www.youtube.com/embed/g408pc2dJNc?si=954iwM5vyAD8CP02",
+  },
+];
